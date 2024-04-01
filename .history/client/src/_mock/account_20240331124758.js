2b8ebdd65c0d429a30e2de10828dd4f17ed20bb5
drumsmanku <mayank.tanwar4444@gmail.com>
2024-04-01 11:48:34 +0530

    final commit

---
+++ .history/client/src/_mock/account_20240331124758.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Mayank Tanwar',
+  email: 'mayank#gmail.com',
+  photoURL: '/assets/images/avatars/avatar_25.jpg',
+};
